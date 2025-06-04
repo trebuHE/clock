f929d8577bfe0d345301332035d0f5132b465c9b
trebuHE <47220041+trebuHE@users.noreply.github.com>
2025-06-04 17:37:50 +0200

    Add top port list

--- clock.srcs/sources_1/new/top.v
+++ clock.srcs/sources_1/new/top.v
@@ -2,6 +2,13 @@
 
 
 module top(
-
+    input wire clk_i,
+    input wire rst_i,
+    input wire button_hr_i,
+    input wire button_min_i,
+    input wire button_test_i,
+    output reg [7:0] led7_seg_o,
+    output reg [7:0] led7_an_o
     );
+    
 endmodule
